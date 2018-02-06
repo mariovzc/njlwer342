92c5c4d56e58ff61f49d63cb5be0d80d42960237
Mario vizcaino <mario.vizcainos@gmail.com>
2018-02-06 10:18:38 -0500

    added task and list elements using map function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      task: '',
+      tasks: ['Sacar la ropa', 'Hacer la cama', 'Leer un rato']
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleText = this.handleText.bind(this)
+  }
   render() {
     return (
-      <div className="wrapper">
-        <div className="list">
+      <div className='wrapper'>
+        <div className='list'>
           <h3>Por hacer:</h3>
-          <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
+          <ul className='todo'>
+            {this.state.tasks.map(element => {
+              return <li key={element}>{element}</li>
+            })}
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
-           </form>
+          <form
+            onSubmit={this.handleSubmit}
+          >
+            <input
+              type='text'
+              id='new-task'
+              onChange={this.handleText}
+              value={this.state.task}
+              placeholder='Ingresa una tarea y oprime Enter'
+            />
+          </form>
         </div>
       </div>
     )
   }
+  handleText (event) {
+    this.setState({task: event.target.value})
+  }
+  handleSubmit (event) {
+    event.preventDefault()
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: ''
+    })
+  }
 }
 
-
-export default App;
+export default App
